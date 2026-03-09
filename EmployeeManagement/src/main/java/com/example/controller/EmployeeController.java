858d1c60bd40d04fff260534697922425c354bd2
abisha170203 <161105312+abisha170203@users.noreply.github.com>
2026-03-09 11:44:29 +0530

    Update EmployeeController.java

--- EmployeeManagement/src/main/java/com/example/controller/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/example/controller/EmployeeController.java
@@ -18,54 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.model.Employee;
 import com.example.serviceIMPL.EmployeeServiceImpl;
 
-
-
-
-//@RestController
-//@RequestMapping("/emp")
-//public class EmployeeController {
-//
-//    @Autowired
-//    private EmployeeServiceImpl empservice;
-//
-//    @GetMapping("/home")
-//    public String homePage() {
-//        return "Welcome to Management System";
-//    }
-//
-//    @PostMapping("/addEmp")
-//    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
-//        Employee emp = empservice.addEmployee(employee);
-//        return new ResponseEntity<>(emp, HttpStatus.CREATED);
-//    }
-//
-//    @DeleteMapping("/removeEmp/{id}")
-//    public ResponseEntity<String> removeEmployee(@PathVariable int id) {
-//        empservice.removeEmployee(id);
-//        return new ResponseEntity<>("Removed Successfully", HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/findEmp/{id}")
-//    public ResponseEntity<Optional<Employee>> findEmployeeById(@PathVariable int id) {
-//        Optional<Employee> emps = empservice.findEmpById(id);
-//        return new ResponseEntity<>(emps, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/listEmp")
-//    public ResponseEntity<List<Employee>> listOfEmployees() {
-//        List<Employee> lEmp = empservice.getAllEmployee();
-//        return new ResponseEntity<>(lEmp, HttpStatus.OK);
-//    }
-//
-//    @PutMapping("/updateEmp/{id}")
-//    public ResponseEntity<String> updateEmployee(@PathVariable int id) {
-//        empservice.updateEmployee(id);
-//        return new ResponseEntity<>("Updated Successfully", HttpStatus.OK);
-//    }
-
-
-
-
 @RestController
 @RequestMapping("/emp")
 public class EmployeeController {
@@ -110,3 +62,4 @@ public class EmployeeController {
         return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
     }
 }
+
